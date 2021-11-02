5e58daecfaf0709ecc9c080b15258dabef651ef6
Rony <rauf.rony@gmail.com>
2021-11-02 19:28:58 +0600

    first commit

---
+++ inc/top.php
@@ -0,0 +1,36 @@
+<body>
+        <div class="container">
+        <a class="fs-1" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><i class="bi bi-list"></i></a>
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">মেনু</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    
+  <ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">মূলপাতা</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ক্যাটাগরি-১</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ক্যাটাগরি-২</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ক্যাটাগরি-৩</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ক্যাটাগরি-৪</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ক্যাটাগরি-৫</a>
+  </li>
+</ul>
+  </div>
+</div>
+
+        </div>
---
+++ index.php
@@ -0,0 +1,274 @@
+<?php
+require_once 'inc/header.php';
+require_once 'inc/top.php';
+?>
+<div class="container">
+  <section>
+    <div class="row">
+      <div class="col-sm-5">
+        <img class="img-fluid" src="uploads/19.jpg" alt="">
+        <h1 class="my-3">৮৪ রানে অলআউট বাংলাদেশ</h1>
+        <p>বাংলাদেশের টি-টোয়েন্টি বিশ্বকাপ কার্যত শেষ। টাইগারদের সেমিফাইনালের আশা বেঁচে আছে কঠিন সমীকরণে। টানা দুই জয়ের সঙ্গে মাহমুদুল্লাহ রিয়াদদের তাকিয়ে থাকতে হবে অন্য দলের ফলের ওপরও। এমন জটিল সমীকরণ মেলাতে নেমে প্রোটিয়া বোলারদের কাছে অসহায় আত্মসমর্পণ করেছে টাইগার ব্যাটাররা। মাত্র ৮৪ রানে হারিয়েছে সবকটি উইকেট...</p>
+      </div>
+      <div class="col-sm-7">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="row">
+              <img class="img-fluid mb-2" src="uploads/18.jpg" alt="">
+              <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            </div>
+            <div class="row">
+              <img class="img-fluid mb-2" src="uploads/17.jpg" alt="">
+              <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            </div>
+            <div class="row">
+              <img class="img-fluid mb-2" src="uploads/15.jpg" alt="">
+              <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="row">
+              <ul class="list-group">
+                <li class="list-group-item active" aria-current="true">সর্বশেষ</li>
+                <li class="list-group-item">গুরুত্বপূর্ণ সফরের আগে কোভিড আক্রান্ত ইরানি পররাষ্ট্রমন্ত্রী</li>
+                <li class="list-group-item">উপনির্বাচনে একতরফা তৃণমূল, বিজেপি ধুয়ে মুছে সাফ</li>
+                <li class="list-group-item">বিশ্বের ক্ষুধা নিবারণে টেসলার শেয়ার বিক্রি করতে রাজি ইলন মাস্ক</li>
+                <li class="list-group-item">কৃষিখাতে বৃটেনের প্রযুক্তিগত সহায়তা চায় বাংলাদেশ</li>
+                <li class="list-group-item">উপনির্বাচনে একতরফা তৃণমূল, বিজেপি ধুয়ে মুছে সাফ</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="row">
+              <img class="img-fluid mb-2" src="uploads/16.jpg" alt="">
+              <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            </div>
+            <div class="row">
+              <img class="img-fluid mb-2" src="uploads/15.jpg" alt="">
+              <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+    <div class="row">
+      <div class="col-sm-3 col-6 mb-3">
+        <img class="img-fluid mb-2" src="uploads/14.jpg" alt="">
+        <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+      </div>
+      <div class="col-sm-3 col-6 mb-3">
+        <img class="img-fluid mb-2" src="uploads/13.jpg" alt="">
+        <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+      </div>
+      <div class="col-sm-3 col-6 mb-3">
+        <img class="img-fluid mb-2" src="uploads/12.jpg" alt="">
+        <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+      </div>
+      <div class="col-sm-3 col-6 mb-3">
+        <img class="img-fluid mb-2" src="uploads/11.jpg" alt="">
+        <h5 class="img-fluid mb-4">৮৪ রানে অলআউট বাংলাদেশ</h5>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+    <h1 class="my-5 text-center border-bottom border-info border-1 pb-3 text-info">মতামত</h1>
+    <div class="row">
+      <div class="col-sm-4 mb-4">
+        <div class="d-flex align-items-center">
+          <div class="flex-shrink-0">
+            <img class="opinion-img img-fluid" src="uploads/op1.jpg" alt="...">
+          </div>
+          <div class="flex-grow-1 ms-3">
+            <h3>৮৪ রানে অলআউট বাংলাদেশ</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="d-flex align-items-center">
+          <div class="flex-shrink-0">
+            <img class="opinion-img img-fluid" src="uploads/op2.jpg" alt="...">
+          </div>
+          <div class="flex-grow-1 ms-3">
+            <h3>৮৪ রানে অলআউট বাংলাদেশ</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="d-flex align-items-center">
+          <div class="flex-shrink-0">
+            <img class="opinion-img img-fluid" src="uploads/op4.jpg" alt="...">
+          </div>
+          <div class="flex-grow-1 ms-3">
+            <h3>৮৪ রানে অলআউট বাংলাদেশ</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="uploads/9.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/8.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/7.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/6.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      <div class="card">
+          <img src="uploads/2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">৮৪ রানে অলআউট বাংলাদেশ</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">নৌকার মনোনয়ন না পেয়ে টাকা ফেরত চাইলেন প্রার্থী</li>
+              <li class="list-group-item">রাজনীতিতে নতুন দল: নানা প্রতিক্রিয়া</li>
+              <li class="list-group-item">যেভাবে টিকে আছে বাংলাদেশের সেমির সম্ভাবনা</li>
+              <li class="list-group-item">শেরপুর সীমান্তে ভারতীয় বন্যহাতির তাণ্ডব</li>
+              <li class="list-group-item">রিজভী-এ্যানীকে দুই মামলা থেকে অব্যাহতির আবেদন</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-md-5">
+                <!--Photo gallery-->
+                <div class="row my-5">
+                    <div class="col-sm-6 mb-3">
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img src="uploads/2.jpg" class="d-block w-100 rounded" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="uploads/3.jpg" class="d-block w-100 rounded" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="uploads/4.jpg" class="d-block w-100 rounded" alt="...">
+                              </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Next</span>
+                            </button>
+                          </div>
+                    </div>
+                <!-- Video gallery -->
+                    <div class="col-sm-6">
+                        <iframe class="rounded" width="100%" height="422" src="https://www.youtube.com/embed/BHACKCNDMW8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </section>
+            
+
+</div>
+
+<?php require_once 'inc/footer.php'; ?>
